1e786e0f984979ba6f482a015eafebb7b9691156
swhah <hsw0311@nate.com>
2018-01-22 18:25:32 +0900

    s

---
+++ 0122.sh
@@ -0,0 +1,508 @@
+/usr/include/asm/e820.h
+/usr/include/asm/mtrr.h
+/usr/include/asm/processor-flags.h
+/usr/include/asm/ipcbuf.h
+/usr/include/asm/sembuf.h
+/usr/include/asm/setup.h
+/usr/include/asm/mce.h
+/usr/include/asm/vm86.h
+/usr/include/asm/unistd_64.h
+/usr/include/asm/signal.h
+/usr/include/asm/errno.h
+/usr/include/asm/ptrace-abi.h
+/usr/include/asm/socket.h
+/usr/include/asm/boot.h
+/usr/include/asm/auxvec.h
+/usr/include/asm/vsyscall.h
+/usr/include/asm/swab.h
+/usr/include/asm/posix_types.h
+/usr/include/asm/sockios.h
+/usr/include/asm/termbits.h
+/usr/include/asm/kvm.h
+/usr/include/asm/byteorder.h
+/usr/include/asm/a.out.h
+/usr/include/asm/sigcontext.h
+/usr/include/asm/resource.h
+/usr/include/asm/ioctls.h
+/usr/include/asm/siginfo.h
+/usr/include/asm/ldt.h
+/usr/include/asm/msgbuf.h
+/usr/include/asm/bootparam.h
+/usr/include/asm/ioctl.h
+/usr/include/asm/stat.h
+/usr/include/asm/bitsperlong.h
+/usr/include/asm/mman.h
+/usr/include/asm/prctl.h
+/usr/include/asm/posix_types_64.h
+/usr/include/asm/fcntl.h
+/usr/include/asm/sigcontext32.h
+/usr/include/asm/msr-index.h
+/usr/include/asm/ptrace.h
+/usr/include/asm/posix_types_32.h
+/usr/include/asm/unistd_32.h
+/usr/include/asm/ist.h
+/usr/include/asm/statfs.h
+/usr/include/thread_db.h
+/usr/include/iconv.h
+/usr/include/grp.h
+/usr/include/glob.h
+/usr/include/netapi.h
+/usr/include/a.out.h
+/usr/include/dlfcn.h
+/usr/include/regexp.h
+/usr/include/fts.h
+/usr/include/fenv.h
+/usr/include/utmp.h
+/usr/include/syscall.h
+/usr/include/pwd.h
+/usr/include/semaphore.h
+/usr/include/mntent.h
+/usr/include/fpu_control.h
+/usr/include/pthread.h
+/usr/include/netpacket
+/usr/include/netpacket/packet.h
+/usr/include/gnu
+/usr/include/gnu/stubs.h
+/usr/include/gnu/libc-version.h
+/usr/include/gnu/lib-names.h
+/usr/include/gnu/stubs-64.h
+/usr/include/ifaddrs.h
+/usr/include/mcheck.h
+/usr/include/malloc.h
+/usr/include/netdb.h
+/usr/include/_G_config.h
+/usr/include/values.h
+/usr/include/stdio_ext.h
+/usr/include/monetary.h
+/usr/include/complex.h
+/usr/include/gconv.h
+/usr/include/regex.h
+/usr/include/sound
+/usr/include/sound/emu10k1.h
+/usr/include/sound/sb16_csp.h
+/usr/include/sound/sfnt_info.h
+/usr/include/sound/asequencer.h
+/usr/include/sound/hdsp.h
+/usr/include/sound/asound_fm.h
+/usr/include/sound/sscape_ioctl.h
+/usr/include/sound/asound.h
+/usr/include/sound/hdspm.h
+/usr/include/re_comp.h
+/usr/include/net
+/usr/include/net/if_slip.h
+/usr/include/net/ppp_defs.h
+/usr/include/net/ethernet.h
+/usr/include/net/if_packet.h
+/usr/include/net/if_arp.h
+/usr/include/net/if_shaper.h
+/usr/include/net/ppp-comp.h
+/usr/include/net/route.h
+/usr/include/net/if.h
+/usr/include/net/if_ppp.h
+/usr/include/aio.h
+/usr/include/nfs
+/usr/include/nfs/nfs.h
+/usr/include/rpc
+/usr/include/rpc/pmap_prot.h
+/usr/include/rpc/types.h
+/usr/include/rpc/pmap_clnt.h
+/usr/include/rpc/rpc.h
+/usr/include/rpc/auth.h
+/usr/include/rpc/key_prot.h
+/usr/include/rpc/auth_des.h
+/usr/include/rpc/clnt.h
+/usr/include/rpc/svc_auth.h
+/usr/include/rpc/rpc_msg.h
+/usr/include/rpc/svc.h
+/usr/include/rpc/rpc_des.h
+/usr/include/rpc/pmap_rmt.h
+/usr/include/rpc/auth_unix.h
+/usr/include/rpc/netdb.h
+/usr/include/rpc/xdr.h
+/usr/include/rpc/des_crypt.h
+/usr/include/stab.h
+/usr/include/resolv.h
+/usr/include/nss.h
+/usr/include/memory.h
+/usr/include/error.h
+/usr/include/inttypes.h
+/usr/include/syslog.h
+/usr/include/stdint.h
+/usr/include/alloca.h
+/usr/include/envz.h
+/usr/include/libgen.h
+/usr/include/uapi
+/usr/include/uapi/nvme.h
+/usr/include/fnmatch.h
+/usr/include/fcntl.h
+/usr/include/ustat.h
+/usr/include/utime.h
+/usr/include/link.h
+/usr/include/mqueue.h
+/usr/include/netipx
+/usr/include/netipx/ipx.h
+/usr/include/assert.h
+/usr/include/netinet
+/usr/include/netinet/ip6.h
+/usr/include/netinet/ip_icmp.h
+/usr/include/netinet/icmp6.h
+/usr/include/netinet/if_ether.h
+/usr/include/netinet/if_tr.h
+/usr/include/netinet/udp.h
+/usr/include/netinet/in_systm.h
+/usr/include/netinet/ip.h
+/usr/include/netinet/igmp.h
+/usr/include/netinet/tcp.h
+/usr/include/netinet/in.h
+/usr/include/netinet/if_fddi.h
+/usr/include/netinet/ether.h
+/usr/include/argp.h
+/usr/include/stdlib.h
+/usr/include/wchar.h
+/usr/include/string.h
+/usr/include/fmtmsg.h
+/usr/include/ctype.h
+/usr/include/features.h
+/usr/include/shadow.h
+/usr/include/wctype.h
+/usr/include/arpa
+/usr/include/arpa/nameser.h
+/usr/include/arpa/inet.h
+/usr/include/arpa/nameser_compat.h
+/usr/include/arpa/ftp.h
+/usr/include/arpa/tftp.h
+/usr/include/arpa/telnet.h
+/usr/include/video
+/usr/include/video/edid.h
+/usr/include/video/sisfb.h
+/usr/include/video/uvesafb.h
+/usr/include/netrose
+/usr/include/netrose/rose.h
+/usr/local
+/usr/local/bin
+/usr/local/lib64
+/usr/local/sbin
+/usr/local/libexec
+/usr/local/etc
+/usr/local/lib
+/usr/local/share
+/usr/local/share/info
+/usr/local/share/applications
+/usr/local/share/applications/mimeinfo.cache
+/usr/local/share/man
+/usr/local/share/man/man9
+/usr/local/share/man/man2
+/usr/local/share/man/man6
+/usr/local/share/man/man5
+/usr/local/share/man/man1x
+/usr/local/share/man/man5x
+/usr/local/share/man/man3
+/usr/local/share/man/man9x
+/usr/local/share/man/man8x
+/usr/local/share/man/man6x
+/usr/local/share/man/man4
+/usr/local/share/man/man8
+/usr/local/share/man/man3x
+/usr/local/share/man/man2x
+/usr/local/share/man/man7x
+/usr/local/share/man/man4x
+/usr/local/share/man/man7
+/usr/local/share/man/man1
+/usr/local/share/man/mann
+/usr/local/src
+/usr/local/games
+/usr/local/include
+/usr/tmp
+/boot
+/boot/initramfs-2.6.32-696.el6.x86_64.img
+/boot/.vmlinuz-2.6.32-696.el6.x86_64.hmac
+/boot/grub
+/boot/grub/device.map
+/boot/grub/menu.lst
+/boot/grub/iso9660_stage1_5
+/boot/grub/grub.conf
+/boot/grub/xfs_stage1_5
+/boot/grub/minix_stage1_5
+/boot/grub/fat_stage1_5
+/boot/grub/jfs_stage1_5
+/boot/grub/stage2
+/boot/grub/vstafs_stage1_5
+/boot/grub/reiserfs_stage1_5
+/boot/grub/stage1
+/boot/grub/ufs2_stage1_5
+/boot/grub/e2fs_stage1_5
+/boot/grub/ffs_stage1_5
+/boot/grub/splash.xpm.gz
+/boot/efi
+/boot/efi/EFI
+/boot/efi/EFI/redhat
+/boot/efi/EFI/redhat/grub.efi
+/boot/vmlinuz-2.6.32-696.el6.x86_64
+/boot/lost+found
+/boot/System.map-2.6.32-696.el6.x86_64
+/boot/symvers-2.6.32-696.el6.x86_64.gz
+/boot/config-2.6.32-696.el6.x86_64
+find: `-': No such file or directory
+find: `name': No such file or directory
+find: `you': No such file or directory
+[root@localhost ~]# - hadoop
+-bash: -: command not found
+[root@localhost ~]# cd
+[root@localhost ~]# hadoop -
+-bash: hadoop: command not found
+[root@localhost ~]# ls
+anaconda-ks.cfg  install.log  install.log.syslog
+[root@localhost ~]# cd /home
+[root@localhost home]# ls
+hadoop
+[root@localhost home]# ls
+hadoop
+[root@localhost home]# cd hadopp
+-bash: cd: hadopp: No such file or directory
+[root@localhost home]# cd hadoop
+[root@localhost hadoop]# ls
+bluesky  Documents  happy  Music   peace     Public      Templates
+Desktop  Downloads  linux  passwd  Pictures  result.txt  Videos
+[root@localhost hadoop]# exit
+logout
+cd[hadoop@localhost ~]$ cd
+[hadoop@localhost ~]$ cd
+[hadoop@localhost ~]$ ls
+Desktop    Downloads  Pictures  Templates  bluesky  linux   peace
+Documents  Music      Public    Videos     happy    passwd  result.txt
+[hadoop@localhost ~]$ cp /backup/grub.conf /home/hadoop/grub.conf
+[hadoop@localhost ~]$ cp /backup/inittab ./
+[hadoop@localhost ~]$ cp /backup/login.defs ./login
+[hadoop@localhost ~]$ cd ls
+bash: cd: ls: 그런 파일이나 디렉터리가 없습니다
+[hadoop@localhost ~]$ ls
+Desktop    Music     Templates  grub.conf  linux   peace
+Documents  Pictures  Videos     happy      login   result.txt
+Downloads  Public    bluesky    inittab    passwd
+[hadoop@localhost ~]$ mkdir homedir
+[hadoop@localhost ~]$ cd
+[hadoop@localhost ~]$ su -
+암호:
+[root@localhost ~]# ls -al
+total 104
+dr-xr-x---.  3 root root  4096 Jan 22 06:52 .
+dr-xr-xr-x. 23 root root  4096 Jan 22 03:45 ..
+-rw-------.  1 root root  3370 Jan 19 08:40 anaconda-ks.cfg
+-rw-------.  1 root root   292 Jan 22 06:38 .bash_history
+-rw-r--r--.  1 root root    18 May 20  2009 .bash_logout
+-rw-r--r--.  1 root root   176 May 20  2009 .bash_profile
+-rw-r--r--.  1 root root   176 Sep 22  2004 .bashrc
+-rw-r--r--.  1 root root   100 Sep 22  2004 .cshrc
+drwx------.  2 root root  4096 Jan 22 06:52 .gconf
+-rw-r--r--.  1 root root 41915 Jan 19 08:40 install.log
+-rw-r--r--.  1 root root  9154 Jan 19 08:36 install.log.syslog
+-rw-r--r--.  1 root root   129 Dec  3  2004 .tcshrc
+-rw-------.  1 root root    66 Jan 22 06:52 .xauthUUzKYp
+[root@localhost ~]# cd /home
+[root@localhost home]# ls
+hadoop
+[root@localhost home]# cd ..
+[root@localhost /]# cd..
+-bash: cd..: command not found
+[root@localhost /]# cd ..
+[root@localhost /]# ls
+backup  boot  etc   lib    lost+found  mnt  proc  sbin     srv  tmp  var
+bin     dev   home  lib64  media       opt  root  selinux  sys  usr
+[root@localhost /]# mkdir /homedir/
+[root@localhost /]# ls
+backup  boot  etc   homedir  lib64       media  opt   root  selinux  sys  usr
+bin     dev   home  lib      lost+found  mnt    proc  sbin  srv      tmp  var
+[root@localhost /]# cd /etc/inittab /homedir/
+-bash: cd: /etc/inittab: Not a directory
+[root@localhost /]# cp /etc/inittab /homedir/
+[root@localhost /]# cp /etc/login.defs /homedir/
+[root@localhost /]# cd boot
+[root@localhost boot]# cd ..
+[root@localhost /]# cd boot
+[root@localhost boot]# ls
+config-2.6.32-696.el6.x86_64         lost+found
+efi                                  symvers-2.6.32-696.el6.x86_64.gz
+grub                                 System.map-2.6.32-696.el6.x86_64
+initramfs-2.6.32-696.el6.x86_64.img  vmlinuz-2.6.32-696.el6.x86_64
+[root@localhost boot]# cd grub
+[root@localhost grub]# ls
+device.map     grub.conf         minix_stage1_5     stage2
+e2fs_stage1_5  iso9660_stage1_5  reiserfs_stage1_5  ufs2_stage1_5
+fat_stage1_5   jfs_stage1_5      splash.xpm.gz      vstafs_stage1_5
+ffs_stage1_5   menu.lst          stage1             xfs_stage1_5
+[root@localhost grub]# cp grub.conf /homedir/
+[root@localhost grub]# cd ..
+[root@localhost boot]# cd ..
+[root@localhost /]# ls
+backup  boot  etc   homedir  lib64       media  opt   root  selinux  sys  usr
+bin     dev   home  lib      lost+found  mnt    proc  sbin  srv      tmp  var
+[root@localhost /]# cd homedir
+[root@localhost homedir]# ;s
+-bash: syntax error near unexpected token `;'
+[root@localhost homedir]# ls
+grub.conf  inittab  login.defs
+[root@localhost homedir]# cp grub.conf /home/hadoop/grub.conf
+cp: overwrite `/home/hadoop/grub.conf'? n
+[root@localhost homedir]# cd /home/hadoop/
+[root@localhost hadoop]# ls
+bluesky    Downloads  homedir  login   peace     result.txt
+Desktop    grub.conf  inittab  Music   Pictures  Templates
+Documents  happy      linux    passwd  Public    Videos
+[root@localhost hadoop]# ls
+bluesky    Downloads  homedir  login   peace     result.txt
+Desktop    grub.conf  inittab  Music   Pictures  Templates
+Documents  happy      linux    passwd  Public    Videos
+[root@localhost hadoop]# cd ..
+[root@localhost home]# cd ..
+[root@localhost /]# ls
+backup  boot  etc   homedir  lib64       media  opt   root  selinux  sys  usr
+bin     dev   home  lib      lost+found  mnt    proc  sbin  srv      tmp  var
+[root@localhost /]# cd homedir
+[root@localhost homedir]# ls
+grub.conf  inittab  login.defs
+[root@localhost homedir]# cd ..
+[root@localhost /]# ls
+backup  boot  etc   homedir  lib64       media  opt   root  selinux  sys  usr
+bin     dev   home  lib      lost+found  mnt    proc  sbin  srv      tmp  var
+[root@localhost /]# cd /home
+[root@localhost home]# cd test
+-bash: cd: test: No such file or directory
+[root@localhost home]# mkdir test
+[root@localhost home]# ls
+hadoop  test
+[root@localhost home]# cd test
+[root@localhost test]# mkdir happy
+[root@localhost test]# mkdir bluesky
+[root@localhost test]# cd bluesky
+[root@localhost bluesky]# mk file01
+-bash: mk: command not found
+[root@localhost bluesky]# cat file01
+cat: file01: No such file or directory
+[root@localhost bluesky]# vi file01
+[root@localhost bluesky]# cat file01
+hahaha
+hohoho
+
+[root@localhost bluesky]# vi file01
+[root@localhost bluesky]# vi file02
+[root@localhost bluesky]# cd ..
+[root@localhost test]# mkdir bluesky
+mkdir: cannot create directory `bluesky': File exists
+[root@localhost test]# ls
+bluesky  happy
+[root@localhost test]# cd bluesky
+[root@localhost bluesky]# ls
+file01  file02
+[root@localhost bluesky]# cd ..
+[root@localhost test]# cd happy
+[root@localhost happy]# ls
+[root@localhost happy]# mkdir fine
+[root@localhost happy]# vi file02
+[root@localhost happy]# cd ..
+[root@localhost test]# cd ..
+[root@localhost home]# cd ..
+[root@localhost /]# ls
+backup  boot  etc   homedir  lib64       media  opt   root  selinux  sys  usr
+bin     dev   home  lib      lost+found  mnt    proc  sbin  srv      tmp  var
+[root@localhost /]# cd homedir
+[root@localhost homedir]# ls
+grub.conf  inittab  login.defs
+[root@localhost homedir]# vi file03
+[root@localhost homedir]# cat > /home/test/bluesky/file01
+^[[A^[[B^[^[^Z
+[1]+  Stopped                 cat > /home/test/bluesky/file01
+[root@localhost homedir]# cat /home/test/bluesky/file01 /home/test/happy/fine/file02 > file03
+cat: /home/test/happy/fine/file02: No such file or directory
+[root@localhost homedir]# cat /home/test/bluesky/file01 /home/test/happy/file02 > file03
+[root@localhost homedir]# cat file03
+hihihi
+[root@localhost homedir]# cat /home/test/bluesky/file01[root@localhost homedir]# cat >  /home/test/bluesky/file01
+vi file01       
+^Z
+[2]+  Stopped                 cat > /home/test/bluesky/file01
+[root@localhost homedir]# cat /home/test/bluesky/file01
+vi file01
+[root@localhost homedir]# cd /home/test/bluesky
+[root@localhost bluesky]# cat file01
+vi file01
+[root@localhost bluesky]# cat > file01
+hahaha
+hohoho^Z
+[3]+  Stopped                 cat > file01
+[root@localhost bluesky]# cat file01
+hahaha
+[root@localhost bluesky]# cat >> file01
+hohoho
+^Z
+[4]+  Stopped                 cat >> file01
+[root@localhost bluesky]# cat file01
+hahaha
+hohoho
+[root@localhost bluesky]# cat /home/test/bluesky/file01 /home/test/happy/file02 > file03
+[root@localhost bluesky]# ls
+file01  file02  file03
+[root@localhost bluesky]# cat file03
+hahaha
+hohoho
+hihihi
+[root@localhost bluesky]# ^C
+[root@localhost bluesky]# cd
+[root@localhost ~]# ls -alR > /home/test/findfile.txt
+[root@localhost ~]# cd /home
+[root@localhost home]# cd test
+[root@localhost test]# ls
+bluesky  findfile.txt  happy
+[root@localhost test]# cat finddile.txt
+cat: finddile.txt: No such file or directory
+[root@localhost test]# cat findfile.txt
+.:
+total 104
+dr-xr-x---.  3 root root  4096 Jan 22 06:52 .
+dr-xr-xr-x. 24 root root  4096 Jan 22 06:53 ..
+-rw-------.  1 root root  3370 Jan 19 08:40 anaconda-ks.cfg
+-rw-------.  1 root root   292 Jan 22 06:38 .bash_history
+-rw-r--r--.  1 root root    18 May 20  2009 .bash_logout
+-rw-r--r--.  1 root root   176 May 20  2009 .bash_profile
+-rw-r--r--.  1 root root   176 Sep 22  2004 .bashrc
+-rw-r--r--.  1 root root   100 Sep 22  2004 .cshrc
+drwx------.  2 root root  4096 Jan 22 08:24 .gconf
+-rw-r--r--.  1 root root 41915 Jan 19 08:40 install.log
+-rw-r--r--.  1 root root  9154 Jan 19 08:36 install.log.syslog
+-rw-r--r--.  1 root root   129 Dec  3  2004 .tcshrc
+-rw-------.  1 root root    66 Jan 22 06:52 .xauthUUzKYp
+
+./.gconf:
+total 8
+drwx------. 2 root root 4096 Jan 22 08:24 .
+dr-xr-x---. 3 root root 4096 Jan 22 06:52 ..
+[root@localhost test]# tail -t find -name find02
+tail: invalid option -- 't'
+Try `tail --help' for more information.
+[root@localhost test]# tail -5 find / -name find02
+tail: option used in invalid context -- 5
+[root@localhost test]# cd
+[root@localhost ~]# tail -5 find / -name find02
+tail: option used in invalid context -- 5
+[root@localhost ~]# find / -name find02
+^Z
+[5]+  Stopped                 find / -name find02
+[root@localhost ~]# find / -name file02
+/home/hadoop/happy/fine/file02
+/home/test/happy/file02
+/home/test/bluesky/file02
+tail [root@localhost ~]# find / -name file02
+/home/hadoop/happy/fine/file02
+/home/test/happy/file02
+/home/test/bluesky/file02
+[root@localhost ~]# > /home/test/findfile.txt
+[root@localhost ~]# cat /home/test/findfile.txt
+[root@localhost ~]# tail find / -name file02 > /hoem/test/findfile.txt
+-bash: /hoem/test/findfile.txt: No such file or directory
+[root@localhost ~]# tail find / -name file02 > /home/test/findfile.txt
+tail: ame: invalid number of lines
+[root@localhost ~]# find / -name file02 > /home/test/findfile.txt
+[root@localhost ~]# cat /home/test/findfile.txt
+/home/hadoop/happy/fine/file02
+/home/test/happy/file02
+/home/test/bluesky/file02
+[root@localhost ~]# find / -name file02 > /home/test/findfile.txt
